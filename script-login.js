bce6319df809c9f056022b99e2914088789bc1db
Whising <86670009+Whisingdilli71@users.noreply.github.com>
2026-02-25 10:16:42 +0530

    Update script-login.js

--- script-login.js
+++ script-login.js
@@ -1,4 +1,9 @@
 document.getElementById("loginForm").addEventListener("submit", (e) => {
     e.preventDefault();
+
+    const username = document.getElementById("username").value;
+
+    alert(`Welcome, ${username}!`);
+
     window.location.href = "./os.html"; 
 });
